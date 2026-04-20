24877c22ddcbffe7cf743d8a4f00e1816dc41b84
mlhmmmdwh505-sys <mlhmmmdwh505@gmail.com>
2026-04-20 20:18:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function playHighVolumeAlarm() {
     source.connect(gainNode);
     gainNode.connect(audioCtx.destination);
 
-    audio.play();
+    playHighVolumeAlarm();
 }
                 
                 addPoints();
