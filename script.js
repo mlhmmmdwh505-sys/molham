b8d9bb13e35b8dc436a27c611dc942dce72eda67
ملهم <mlhmmmdwh505@gmail.com>
2026-07-21 14:30:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -489,3 +489,10 @@ function renderTasks() {
         taskList.appendChild(li);
     });
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log('Service Worker Registered Successfully!'))
+      .catch(err => console.log('Service Worker Registration Failed: ', err));
+  });
+}
