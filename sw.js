308e48c9f57b545b1bf8b215a8e47e45799a2e54
ملهم <mlhmmmdwh505@gmail.com>
2026-08-10 14:46:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'surgeon-dashboard-v4';
+const CACHE_NAME = 'surgeon-dashboard-v5';
 
 // الأصول والملفات المطلوبة للتخزين حتى يعمل الموقع بدون إنترنت
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './style.css?v=8',
+  './style.css?v=9',
   './script.js?v=14',
   './manifest.json?v=8',
   './gnome-books.svg',
